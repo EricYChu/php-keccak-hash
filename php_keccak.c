78706e416b6ec1d1972b2b7b59c15b54998c9a47
Eric Chu <ericychu@me.com>
2018-04-04 23:30:24 +0800

    Added raw output option

--- php_keccak.c
+++ php_keccak.c
@@ -6,6 +6,7 @@
 #include "php_ini.h"
 #include "ext/standard/info.h"
 #include "ext/standard/basic_functions.h"
+#include "ext/hash/php_hash.h"
 #include "KeccakNISTInterface.h"
 #include "php_keccak.h"
 
@@ -56,32 +57,54 @@ PHP_MINFO_FUNCTION(keccak)
 
 PHP_FUNCTION(keccak_hash)
 {
-   char *buffer = NULL;
-   char hash[64];
-   int buffer_size;
-   long hash_bit_length = KECCAK_DEFAULT_BIT_LENGTH;
-
-   if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|l",
-            &buffer, &buffer_size, &hash_bit_length) == FAILURE) 
-   {
-      RETURN_FALSE;
-   }
-   
-   memset(hash, 0, sizeof hash);
-   HashReturn result;
-
-   if ((result = Hash((size_t) hash_bit_length, buffer, buffer_size * 8, hash)) != HASH_SUCCESS)
-   {
-      if (result == BAD_HASHLEN)
-      {
-         php_error_docref(NULL TSRMLS_CC, E_WARNING, "Bad bit-length");
-      }
-
-      RETURN_FALSE;
-   }
 #if ZEND_MODULE_API_NO >= 20151012
-   RETVAL_STRINGL(hash, ceil(hash_bit_length / 8));
+    zend_long hash_bit_length = KECCAK_DEFAULT_BIT_LENGTH;
+    zend_long hash_byte_length;
+    size_t buffer_size;
 #else
-   RETURN_STRINGL(hash, ceil(hash_bit_length / 8), 1);
+    long hash_bit_length = KECCAK_DEFAULT_BIT_LENGTH;
+    long hash_byte_length;
+    int buffer_size;
 #endif
+    BitSequence *buffer = NULL;
+    BitSequence hash[64];
+    zend_bool raw_output = 0;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|lb", &buffer, &buffer_size, &hash_bit_length, &raw_output) == FAILURE)
+    {
+        RETURN_FALSE;
+    }
+
+    memset(hash, 0, sizeof hash);
+    HashReturn result;
+
+    if ((result = Hash((size_t) hash_bit_length, buffer, buffer_size * 8, hash)) != HASH_SUCCESS)
+    {
+        if (result == BAD_HASHLEN)
+        {
+            php_error_docref(NULL TSRMLS_CC, E_WARNING, "Bad bit-length");
+        }
+
+        RETURN_FALSE;
+    }
+
+    hash_byte_length = ceil(hash_bit_length / 8);
+
+    if (raw_output) {
+#if ZEND_MODULE_API_NO >= 20151012
+        RETVAL_STRINGL((char *)hash, hash_byte_length);
+#else
+        RETURN_STRINGL((char *)hash, hash_byte_length, 1);
+#endif
+    } else {
+        char *hexDigest = safe_emalloc(hash_byte_length, 2, 1);
+
+        php_hash_bin2hex(hexDigest, hash, hash_byte_length);
+        hexDigest[2 * hash_byte_length] = 0;
+#if ZEND_MODULE_API_NO >= 20151012
+        RETVAL_STRINGL(hexDigest, hash_byte_length * 2);
+#else
+        RETURN_STRINGL(hexDigest, hash_byte_length * 2, 1);
+#endif
+    }
 }
